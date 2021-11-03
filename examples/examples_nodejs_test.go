aeedb0b754b0c8bae7a261aceaebb2fdd926da39
stack72 <public@paulstack.co.uk>
2021-11-03 13:40:36 +0000

    Setup acceptance tests to ensure correct dependencies

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -23,7 +23,7 @@ func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions()
 	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
-			"@pulumi/sumologic",
+			"@pulumi/ec",
 		},
 	})
 
